53e6c79baea3815ed8a1a9e6b3bd5a8323f29a33
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-05-06 15:03:40 -0400

    Update album.js

--- photo-albums/album.js
+++ photo-albums/album.js
@@ -1,3 +1,6 @@
+let photoArr = document.querySelectorAll(".photo-container img");
+
+let currentIndex = 0;
 
 
 function showBig(_self) {
@@ -7,8 +10,41 @@ function showBig(_self) {
   
   BIG_DIV.style.display = "block";
   BIG_IMG.setAttribute("src", _self.src);
+  
+  for (let _i = 0; _i < photoArr.length; _i++) {
+    if (photoArr[_i].isEqualNode(_self));
+}
+  
 }
 
 function hideBig(_self) {
   _self.style.display = "none";
+}
+
+window.addEventListener("keydown", (_key) => {
+  if (_key.code == "ArrowRight") increaseIndex();
+  else if (_key.code == "ArrowLeft") decreaseIndex();
+  updateBig();
+});
+
+function increaseIndex() {
+  currentIndex++;
+  if (currentIndex >= photoArr.length) currentIndex = 0;  
+}
+
+function decreaseIndex() {
+  currentIndex--;
+  if (currentIndex < 0) currentIndex = photoArr.length - 1;
+}
+
+function updateBig() {
+  const BIG_IMG = document.getElementById("big-img");
+  const BIG_DIV = document.getElementById("big-container");
+  
+  BIG_IMG.setAttribute("src", photoArr[currentIndex]);
+  if (BIG_DIV.style.display == "block") {
+    BIG_DIV.style.display = "none";
+    BIG_DIV.offsetHieght;
+    BIG_DIV.style.display = "block";
+  }
 }
